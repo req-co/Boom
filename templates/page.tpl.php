7b656353d11963ace32ff5f5024588a0413806c2
Mitchell Shelton <mshelton@repequity.com>
2015-03-09 09:22:25 -0400

    Add basic regions and related divs

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,4 +1,18 @@
+<header class="header" id="header" role="header">
+  <div class="header-content">
+    <?php print render($page['header']); ?>
+  </div>
+</header>
+
 <main class="main" id="main" role="main">
-  <?php print $messages; ?>
-  <?php print render($page['content']); ?>
+  <div class="main-content">
+    <?php print $messages; ?>
+    <?php print render($page['content']); ?>
+  </div>
 </main>
+
+<footer class="footer" id="footer" role="footer">
+  <div class="footer-content">
+    <?php print render($page['footer']); ?>
+  </div>
+</footer>
